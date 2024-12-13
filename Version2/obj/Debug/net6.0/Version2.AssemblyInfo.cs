64acd6dfee2c21a3855020bb042812b944bbfd5c
Khoa Truong <thukhoa1011@gmail.com>
2024-12-14 03:30:41 +0700

    Commit để merge giao diện phiếu đặt hàng

--- Version2/obj/Debug/net6.0/Version2.AssemblyInfo.cs
+++ Version2/obj/Debug/net6.0/Version2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Version2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f1a890b7fe4f3ee882705fbff67bfe19fa49a4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+196f7eb382c4233a5d20056ae9d539193cf8dd03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Version2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Version2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
